9098f7ee4080502792107b57b57bd0777ac997ec
Vinyks <54633020+Vinyks@users.noreply.github.com>
2026-05-10 02:32:50 +0200

    Added Dataset and list of valveGames

---
+++ Main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('dataset.csv', encoding='utf-8')
+
+valveGames = ["Portal 2", "Left 4 Dead", "Portal", "Counter-Strike",
+              "Counter-Strike: Source", "Half-Life", "Team Fortress Classic", "Day of Defeat: Source",
+              "Day of Defeat", "Ricochet", "Deathmatch Classic",
+              "Half-Life 2: Deathmatch", "Half-Life Deathmatch: Source", "Alien Swarm",
+              "Counter-Strike: Condition Zero", "Half-Life: Source",
+              "Half-Life 2", "Half-Life 2: Lost Coast", "Half-Life 2: Episode One", "Half-Life 2: Episode Two",
+              "Left 4 Dead 2", "Dota 2", "The Lab", "Team Fortress 2"]
+
